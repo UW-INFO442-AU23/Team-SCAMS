24204812bb1fe82bfd48cee4f81b0fccc31fce49
frogboba <schu18@uw.edu>
2023-10-23 14:04:07 -0700

    added component imports to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import Navbar from './components/Navbar/Navbar.js';
+import Home from './components/Home/Home.js';
+import Quiz from './components/Quiz/Quiz.js';
+import Calculator from './components/Calculator/Calculator.js';
+import ResourcePage from './components/ResourcePage/ResourcePage.js';
+import ResourceCard from './components/ResourceCard/ResourceCard.js';
 
 export default function App (props) {
 
