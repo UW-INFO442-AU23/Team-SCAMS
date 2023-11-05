8c4d9459ecceab83b1ffe79ec6662a370b801876
Aly1226 <jyu22@uw.edu>
2023-11-05 11:44:03 -0800

    fix router aand update database

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './components/Home.js';
 import Quiz from './components/Quiz.js';
 import Calculator from './components/Calculator.js';
 import ResourcePage from './components/ResourcePage.js';
-import ResourceCard from './components/ResourceCard.js';
+// import ResourceCard from './components/ResourceCard.js';
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 
 export default function App (props) {
