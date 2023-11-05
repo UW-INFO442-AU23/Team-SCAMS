2d87dff9605ee7035befd06cd987eab6ad9cedcf
Mariko Wodoworth <mariwoodworth@gmail.com>
2023-11-04 18:27:44 -0700

    added resource image grid cards

--- src/components/ResourceCard.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-
-export default function ResourceCard (props) {
-    <Route path="/ResourceCard" element={<ResourceCard/>} />
-}   
