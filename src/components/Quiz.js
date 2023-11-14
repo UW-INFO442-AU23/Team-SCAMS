5ed27e4898e512971362276a12ea27f0b28fbaeb
frogboba <schu18@uw.edu>
2023-11-14 01:22:06 -0800

    submit quiz button functionality

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -5,7 +5,6 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-d
 function QuizResult(props) {
     const { answers } = props
     const correct = ["Q1c", "Q2c", "Q3b", "Q4d", "Q5a"]
-
     let count = 0
     const answers_array = Object.values(answers)
 
@@ -18,69 +17,71 @@ function QuizResult(props) {
         }
     }
 
+    const result = count / correct.length
+    console.log(result);
 
-return (
-    <div>
-        <div>
-            <h1 class="text-center">Your Quiz Results</h1>
-        </div>
-
-        <div class="container quiz_question">
-            <h2>Congrats on finishing the quiz!</h2>
-        </div>
-
-        <div class="flex-container">
-
-            <div class="your-score">
-                <h3>Your Score:</h3>
+    return (
+        <div style={{ visibility: props.status ? "visible" : "hidden" }}>
+            <div>
+                <h1 class="text-center">Your Quiz Results:</h1>
             </div>
 
-            <div class="correct-answers">
-                <h3>Correct Answers:</h3>
-                <h4>
-                    Q1: The Link light rail travels between which two stops?
-                </h4>
-                <p>
-                    Answer: C. Northgate and Angle Lake
-                </p>
-                <a href="https://www.soundtransit.org/ride-with-us/stations/link-light-rail-stations">Want more info on the available stations? Find out here!</a>
+            <div class="container quiz_question">
+                <h2>Congrats on finishing the quiz!</h2>
+            </div>
 
-                <h4>
-                    Q2: Walking ___ steps a day (around 3-4 miles) can reduce risk of heart disease.
-                </h4>
-                <p>
-                    Answer: C. 8,000 steps
-                </p>
-                <a href="https://www.health.harvard.edu/heart-health/step-up-your-walking-game">Click to read the Harvard study.</a>
+            <div class="flex-container">
 
-                <h4>
-                    Q3: True or false?: Carbon Monoxide is the most prevalent greenhouse gas.
-                </h4>
-                <p>
-                    Answer: B. False
-                </p>
-                <a href="https://www.epa.gov/ghgemissions/overview-greenhouse-gases">It's carbon dioxide!</a>
+                <div class="your-score">
+                    <h3>Your Score: {result * 100}% correct</h3>
+                </div>
 
-                <h4>
-                    Q4: King County Metro carries over ___ passengers daily.
-                </h4>
-                <p>
-                    Answer: D. 400,000
-                </p>
-                <a href="https://en.wikipedia.org/wiki/King_County_Metro#cite_note-METRO_Mag_Survey-3">Read the King County Metro's Wiki page!</a>
+                <div class="correct-answers">
+                    <h3>Correct Answers:</h3>
+                    <h4>
+                        Q1: The Link light rail travels between which two stops?
+                    </h4>
+                    <p>
+                        Answer: C. Northgate and Angle Lake
+                    </p>
+                    <a href="https://www.soundtransit.org/ride-with-us/stations/link-light-rail-stations">Want more info on the available stations? Find out here!</a>
 
-                <h4>
-                    Q5: Emissions from the transportation sector accounts for what percentage of total U.S. CO2
-                    emissions?
-                </h4>
-                <p>
-                    Answer: A. 35%
-                </p>
-                <a href="https://www.epa.gov/ghgemissions/overview-greenhouse-gases">Where we got this stat:</a>
+                    <h4>
+                        Q2: Walking ___ steps a day (around 3-4 miles) can reduce risk of heart disease.
+                    </h4>
+                    <p>
+                        Answer: C. 8,000 steps
+                    </p>
+                    <a href="https://www.health.harvard.edu/heart-health/step-up-your-walking-game">Click to read the Harvard study.</a>
+
+                    <h4>
+                        Q3: True or false?: Carbon Monoxide is the most prevalent greenhouse gas.
+                    </h4>
+                    <p>
+                        Answer: B. False
+                    </p>
+                    <a href="https://www.epa.gov/ghgemissions/overview-greenhouse-gases">It's carbon dioxide!</a>
+
+                    <h4>
+                        Q4: King County Metro carries over ___ passengers daily.
+                    </h4>
+                    <p>
+                        Answer: D. 400,000
+                    </p>
+                    <a href="https://en.wikipedia.org/wiki/King_County_Metro#cite_note-METRO_Mag_Survey-3">Read the King County Metro's Wiki page!</a>
+
+                    <h4>
+                        Q5: Emissions from the transportation sector accounts for what percentage of total U.S. CO2
+                        emissions?
+                    </h4>
+                    <p>
+                        Answer: A. 35%
+                    </p>
+                    <a href="https://www.epa.gov/ghgemissions/overview-greenhouse-gases">Where we got this stat:</a>
+                </div>
             </div>
         </div>
-    </div>
-)
+    )
 }
 
 
@@ -132,7 +133,6 @@ export default function Quiz(props) {
     const { data } = props
 
     const [answers, setAnswers] = useState([]);
-
     const handleAnswer = (questionId, answer) => {
         setAnswers((prevAnswers) => ({
             ...prevAnswers,
@@ -145,16 +145,19 @@ export default function Quiz(props) {
         return <QuizQuestion quizData={question} key={index} onSelect={handleAnswer} />
     })
 
+    const [show, setShow] = useState(false);
+
+
     return (
         <main>
             <div className='quiz_background'>
                 <h1 className="text-center">Knowledge Quiz</h1>
                 {questionArray}
             </div>
-            <div className="button_center">
-                <button type="button" className="btn btn-primary my-5">Submit</button>
-            </div>
-            <QuizResult answers={answers} />
+            <button type="button" className="btn btn-primary my-5" onClick={() => setShow(!show)}>
+                {show ? "Hide" : "View"} results
+            </button>
+            <QuizResult answers={answers} status={show} />
         </main>
     )
 
