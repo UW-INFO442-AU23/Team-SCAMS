339d9493d777e2da6898cdbf5df51e927f8c2e9a
Mariko Wodoworth <mariwoodworth@gmail.com>
2023-11-14 13:39:54 -0800

    edited resource page

--- src/components/ResourcePage.js
+++ src/components/ResourcePage.js
@@ -23,7 +23,7 @@ export function Resource (props) {
         <div className='container'> 
             <Title heading1={'Resource Page'} />
         
-            <h2>What Is A Carbon Footprint?</h2>
+            <h2>What is a Carbon Footprint?</h2>
             <p>
                 According to the <a
                     href="https://www.nationalgeographic.com/environment/article/what-is-a-carbon-footprint-how-to-measure-yours?loggedin=true&rnd=1698131599474">National
@@ -38,12 +38,12 @@ export function Resource (props) {
             </p>
 
             <div class="container callout_container rounded py-4 my-5">
-                <h2 class="text-center">Green Tip</h2>
-                <p class="text-center">Are you viewing this website on your computer through Chrome? Did you know that <a
+                
+                <h2 class="text-center">Green Tip!</h2>
+                <p>Are you viewing this website on your computer through Chrome? Did you know that <a
                         href="https://googleblog.blogspot.com/2009/01/powering-google-search.html">every
-                        Google search produces around 0.2 grams of carbon dioxide</a>? Here are a couple of alternate web
-                    browsers
-                    that are eco-friendlier and help offset your carbon footprint using money generated from ad revenue:
+                        Google search produces around 0.2 grams of carbon dioxide</a>? Here are a couple of alternate web browsers
+                        that are eco-friendlier and help offset your carbon footprint using money generated from ad revenue:
                 </p>
                 <li>
                     <a href="https://www.ecosia.org/?c=en">Ecosia:</a> Plants trees
@@ -57,7 +57,7 @@ export function Resource (props) {
             </div>
 
             <h2>Alternate Forms of Transportation</h2>
-            <p class="text-center">
+            <p>
                 What’s one way that you can reduce your carbon footprint? If you commute regularly by driving yourself in a
                 car, a good first step can be how you get around King County. According to the <a
                     href="https://www.epa.gov/ghgemissions/overview-greenhouse-gases">US Environmental Protection
@@ -68,13 +68,16 @@ export function Resource (props) {
                 greenhouse gas emissions. Click on each card below to learn more about each method of eco-friendlier,
                 sustainable modes of transportation that are available in the Seattle area.
             </p>
+            
+            <div class="py-4">
+                <h4 class="text-center">Take our Knowledge Quiz to see how much you know!</h4>
 
-            <p class="text-center">Take our Knowledge Quiz to see how much you know!</p>
-
-            <div class="button_center">
-                <button type="button" class="btn btn-primary">Take The Quiz</button>
+                <div class="button_center">
+                    <button type="button" class="btn btn-primary">Take The Quiz</button>
+                </div>
             </div>
             
+            <ResourceCard />
         </div>
 
     )
