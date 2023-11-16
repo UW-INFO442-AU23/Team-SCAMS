06eae2bff1ad16556420b8e0e2ab7f66c9673d8d
clairegl <cglai@uw.edu>
2023-11-15 16:16:59 -0800

    nav spacing fix and router fix

--- src/App.js
+++ src/App.js
@@ -7,19 +7,20 @@ import ResourcePage from './components/ResourcePage.js';
 import QuizData from './data/Quiz.json'
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 
-export default function App (props) {
-    return (
-        <div>
-          <NavbarComponent />
-            <Routes>
-                <Route path='/home' element={<Home/>}/>
-                <Route path='/emissions_calculator' element={<Calculator/>}/>
-                <Route path='/knowledge_quiz' element={<Quiz data={QuizData}/>}/>
-                <Route path='/resources' element={<ResourcePage/>}/>
-            </Routes>
-          <Footer/>
-        </div>
-      );
+export default function App(props) {
+  return (
+    <div>
+      <NavbarComponent />
+      <Routes>
+        <Route path='*' element={<Navigate to='/home' />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/emissions_calculator' element={<Calculator />} />
+        <Route path='/knowledge_quiz' element={<Quiz data={QuizData} />} />
+        <Route path='/resources' element={<ResourcePage />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 function Footer() {
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,15 +9,15 @@ export default function NavbarComponent(props) {
         <Navbar expand="lg" variant="light">
             <Container>
                 <Navbar.Brand as={Link} to='home'>
-                    <img src="logo.png" alt="go green logo" width="100" height="100" />
+                    <img src="logo.png" alt="go green logo" width="150" height="150" />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className='ms-auto'>
-                    <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                    <Nav.Link className='nav_click' as={Link} to="/home">Home</Nav.Link>
                     <Nav.Link as={Link} to='/emissions_calculator'>Emissions Calculator</Nav.Link>
+                    <Nav.Link activeStyle={{color:'#006D04'}} as={Link} to='/resources'>Resources</Nav.Link>
                     <Nav.Link as={Link} to='/knowledge_quiz'>Knowledge Quiz</Nav.Link>
-                    <Nav.Link as={Link} to='/resources'>Resources</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
