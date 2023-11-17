658ccd218cb3e584b54a77595f2cfa95280e756d
clairegl <cglai@uw.edu>
2023-11-17 11:56:42 -0800

    nav tell you what page ur on

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate, Link, NavLink} from "react-router-dom";
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import { Nav } from 'react-bootstrap';
@@ -14,10 +14,10 @@ export default function NavbarComponent(props) {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className='ms-auto'>
-                    <Nav.Link className='nav_click' as={Link} to="/home">Home</Nav.Link>
-                    <Nav.Link as={Link} to='/emissions_calculator'>Emissions Calculator</Nav.Link>
-                    <Nav.Link activeStyle={{color:'#006D04'}} as={Link} to='/resources'>Resources</Nav.Link>
-                    <Nav.Link as={Link} to='/knowledge_quiz'>Knowledge Quiz</Nav.Link>
+                    <Nav.Link className='nav-link' as={NavLink} to="/home" >Home</Nav.Link>
+                    <Nav.Link className='nav-link' as={NavLink} to='/emissions_calculator'>Emissions Calculator</Nav.Link>
+                    <Nav.Link className='nav-link' as={NavLink} to='/resources'>Resources</Nav.Link>
+                    <Nav.Link className='nav-link' as={NavLink} to='/knowledge_quiz'>Knowledge Quiz</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
