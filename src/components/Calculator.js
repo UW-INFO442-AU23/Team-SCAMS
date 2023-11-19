e58cd206b966497472e7e8f815fbe9e186ccb94e
Aly1226 <jyu22@uw.edu>
2023-11-19 14:30:54 -0800

    fix input bar

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,63 +3,20 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-d
 import Data from '../data/CarbonRate.json';
 
 
-export default function Calculator(props) {
+export default function Calculator() {
   <Route path="/Calculator" element={<Calculator />} />
+  const [miles, setMiles] = useState("");
+  const [transportation, setTransportation] = useState("Walk");
+  let [output, setOutput] = useState([]);
 
-  return (
-    <main>
-      <div className="container">
-        <div className="row align-items-start">
-          <div className="col">
-            <h1 className='pb-2'>Check Your Carbon Emissions</h1>
-            <Calculating />
-            <p>Check our data source
-              <a href="https://www.iea.org/data-and-statistics/charts/well-to-wheel-wake-wing-ghg-intensity-of-motorised-passenger-transport-modes-2" target="_blank"> here </a>
-            </p>
-          </div>
-          <div className='col'>
-            <Result />
-          </div>
-        </div>
-      </div>
-    </main>
-  )
-}
-
-
-// implement calculating feature on the left side of the page
-function Calculating(props) {
-  const [miles, setMiles] = useState("")
-  const [transportation, setTransportation] = useState("Walk")
-
-  // get mode of transportation from dataset for the dropdown menu
-  function DropdownMenu() {
-    let transportationModes = [];
-    for (var i = 0; i < Data.length; i++) {
-      transportationModes.push(Data[i].transportation)
-    }
-    let options = transportationModes.map(optionFunction)
-    function optionFunction(option) {
-      return (
-        <option value={option}>{option}</option>
-      )
-    }
-    return (
-      <div className="form-group">
-        <label for="transportation-options">How will you get there?</label>
-        <select id="transportation-options" name="transportation-options" className="form-control" onChange={handleTransportationChange} required>
-          {options}
-        </select>
-      </div>
-    )
-  }
-
+  // handle user input miles (num input)
   const handleMilesChange = (event) => {
     let inputValue = event.target.value;
     setMiles(inputValue)
     console.log(inputValue)
   }
 
+  // handle user input mode of transportation (option selection)
   const handleTransportationChange = (event) => {
     let inputValue = event.target.value;
     setTransportation(inputValue)
@@ -77,37 +34,52 @@ function Calculating(props) {
   function calculatingCallback() {
     let carbonEmission = [];
     for (var i = 0; i < Data.length; i++) {
-      carbonEmission.push(Data[i].rate * miles)
+      carbonEmission.push(Math.round(Data[i].rate * miles))
     }
     console.log(carbonEmission)
   }
 
   return (
-    <div className="form-container">
-      <div className="form-content">
-        <form>
-          <section className="form-group">
-            <label for="mileage">How far are you traveling? (in Miles)</label>
-            <input type="text" id="mileage" name="mileage" className="form-control" onChange={handleMilesChange} required />
-          </section>
-          <DropdownMenu />
-          <div className="form-group">
-            <button aria-label="Submit" className="btn btn-primary" onClick={handleSubmit} >See Results</button>
+    <main>
+      <div className="container">
+        <div className="row align-items-start">
+          <div className="col">
+            <h1 className='pb-2'>Check Your Carbon Emissions</h1>
+            <div className="form-container">
+              <div className="form-content">
+                <form>
+                  <section className="form-group">
+                    <label for="mileage">How far are you traveling? (in Miles)</label>
+                    <input type="text" id="mileage" name="mileage" className="form-control" onChange={handleMilesChange} required />
+                  </section>
+                  <div className="form-group">
+                    <label for="transportation-options">How will you get there?</label>
+                    <select value={transportation} id="transportation-options" name="transportation-options" className="form-control" onChange={handleTransportationChange} required>
+                      <option value="" disabled>Select an option</option>
+                      {Data.map((item) => (
+                        <option key={item.id} value={item.value}>
+                          {item.transportation}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  <div className="form-group">
+                    <button aria-label="Submit" className="btn btn-primary" onClick={handleSubmit} >See Results</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <p>Check our data source
+              <a href="https://www.iea.org/data-and-statistics/charts/well-to-wheel-wake-wing-ghg-intensity-of-motorised-passenger-transport-modes-2" target="_blank"> here </a>
+            </p>
           </div>
-        </form>
+          <div className='col'>
+            <div className='container bg-light result'>
+              <img src="img/lightrail.png" alt="Washington State Lightrail" width="600px" />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  )
-}
-
-// implement the result on the right side of the page
-function Result(props) {
-  let miles = props.miles;
-  let transportation = props.transportation
-
-  return (
-    <div className='container bg-light'>
-      <img src="img/lightrail.png" alt="Washington State Lightrail" width="700px" />
-    </div>
+    </main>
   )
 }
