331aac3c51510cf6cdeb92c3345ca9c1c7eef6e3
Mariko Wodoworth <mariwoodworth@gmail.com>
2023-11-22 11:50:36 -0800

    edited modal for cards

--- src/components/ResourcePage.js
+++ src/components/ResourcePage.js
@@ -19,14 +19,30 @@ function Title(props) {
     )
 }
 
+
+const Modal = ({ title, image, onClose }) => (
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        <img src={image} alt={title} />
+        <h2>{title}</h2>
+        <button onClick={onClose}>Close</button>
+      </div>
+    </div>
+  );
+
 export function ResourceCard(props) {
     const title = props.title;
     const image = props.image;
     const alt = props.alt;
 
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
+
     return (
         <div className="d-flex col-md-6 col-lg-4 px-4 py-3">
-            <div className='card mb-4'>
+            <div className='card mb-4' onClick={openModal}>
                 <div className="entry-card card-body">
                     <div className="col-sm-auto">
                         <img className="col-sm-12" id="homecardpic" src={image} alt={alt} />
@@ -34,12 +50,15 @@ export function ResourceCard(props) {
                     <div className="col-sm">
                         <h2 className="card-title homeTitle">{title}</h2>
                     </div>
+                    {isModalOpen && (
+                    <Modal title={title} image={image} onClose={closeModal} />
+                    )}
                 </div>
             </div>
         </div>
     );
 }
-
+  
   
 export function EntryCardList(props) {
     const links = props.links;
